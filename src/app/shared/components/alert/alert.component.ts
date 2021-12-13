93ce54f90d7efc01008a06f12621d42ef35c9a6a
josellia <joseliaapires@hotmail.com>
2021-12-13 18:27:12 -0300

    feat: componente modal

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -11,12 +11,22 @@ export class AlertComponent implements OnInit {
   title = 'Sucesso!';
   descriptionModal = 'Seu registro foi cadastrado com sucesso!';
   btnSuccess = 'Ok';
-  btnCancel = 'Cancelar'
+  btnCancel = 'Cancelar';
+  colorBtn = 'primary';
+  isBtnClose = false;
 
   constructor( public dialogRef: MatDialogRef<AlertComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+   if(this.data) {
+     this.title = this.data.title || this.title;
+     this.descriptionModal = this.data.descriptionModal || this.descriptionModal;
+     this.btnSuccess = this.data.btnSuccess || this.btnSuccess;
+     this.btnCancel = this.data.btnCancel || this.btnCancel;
+     this.colorBtn = this.data.colorBtn || this.colorBtn;
+     this.isBtnClose = this.data.isBtnClose || this.isBtnClose;
+   }
   }
 
 }
